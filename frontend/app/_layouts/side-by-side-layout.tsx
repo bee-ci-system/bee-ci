5d007c621172bfbdf3479f57892f6dae565e048c
Bartek Pacia <barpac02@gmail.com>
2024-08-01 00:27:09 +0200

    SideBySideLayout: add Sign in with GitHub button

--- frontend/app/_layouts/side-by-side-layout.tsx
+++ frontend/app/_layouts/side-by-side-layout.tsx
@@ -5,9 +5,12 @@ import { Button } from '../_components/button';
 import { GithubIcon } from '../_icons/github-icon';
 import { staticAssets } from '../_utils/static-assets';
 
-export const SideBySideLayout = ({ children }: { children: ReactNode }) => (
-  <div className='h-screen'>
-    <div className='flex h-full grow flex-col md:flex-row'>
+export const SideBySideLayout = ({ children }: { children: ReactNode }) => {
+  const clientId = process.env.NEXT_PUBLIC_GITHUB_APP_CLIENT_ID;
+  const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}`;
+
+  return (
+    <div className='flex h-screen grow flex-col md:flex-row'>
       <div className='animated-background flex h-full flex-col justify-center gap-6 border-b-[1px] border-beeci-yellow-600 bg-gradient-to-br from-gray-950 from-10% via-gray-900 via-80% to-beeci-yellow-950 to-100% px-14 pb-10 pt-16 md:w-5/12 md:border-b-0 md:border-r-[1px]'>
         <div>
           <Image
@@ -29,7 +32,9 @@ export const SideBySideLayout = ({ children }: { children: ReactNode }) => (
             efficient, user-friendly, and designed to make your development
             lifecycle buzz 🐝 with productivity.
           </p>
-          <Button className='mt-6 w-1/2'>Begin here </Button>
+          <Button variant='link' className='mt-6 w-1/2'>
+            <a href={authUrl}>Sign in with GitHub</a>
+          </Button>
         </div>
         <div className='flex w-full flex-col items-start text-sm md:flex-row md:gap-4 lg:gap-8'>
           <Button variant='link' className='px-0'>
@@ -52,5 +57,5 @@ export const SideBySideLayout = ({ children }: { children: ReactNode }) => (
         <div className='h-full p-4'>{children}</div>
       </div>
     </div>
-  </div>
-);
+  );
+};
