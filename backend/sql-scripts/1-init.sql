aaf5c5d37ca284ff60c6d146d4f578fdc13f32fe
Bartek Pacia <barpac02@gmail.com>
2024-08-04 17:32:54 +0100

    create SQL db init script

---
+++ backend/sql-scripts/1-init.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE bee;
+
+USE bee;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY, -- GitHub user id
+    username VARCHAR(255) UNIQUE NOT NULL, -- GitHub username
+    installation_token VARCHAR(40) NOT NULL
+);
+
+CREATE TABLE builds (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES,
+    repo VARCHAR(255) NOT NULL,
+    branch VARCHAR(255) NOT NULL,
+    commit VARCHAR(40) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
