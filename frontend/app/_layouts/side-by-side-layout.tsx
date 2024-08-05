3e7ec3ac508befb4a1d64c4ae5bb170499bbe09e
Aleksandra Kacprzak <aleksandra.kacprzak@gorrion.io>
2024-08-05 21:51:38 +0200

    feat/added changes from kacaleksandra/bee-ci

--- frontend/app/_layouts/side-by-side-layout.tsx
+++ frontend/app/_layouts/side-by-side-layout.tsx
@@ -5,9 +5,12 @@ import { Button } from '../_components/button';
 import { GithubIcon } from '../_icons/github-icon';
 import { staticAssets } from '../_utils/static-assets';
 
-export const SideBySideLayout = ({ children }: { children: ReactNode }) => (
-  <div className='h-screen'>
-    <div className='flex h-full grow flex-col md:flex-row'>
+export const SideBySideLayout = ({ children }: { children: ReactNode }) => {
+  const clientId = process.env.NEXT_PUBLIC_GITHUB_APP_CLIENT_ID;
+  const authUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}`;
+
+  return (
+    <div className='flex h-screen grow flex-col md:flex-row'>
       <div className='animated-background flex h-full flex-col justify-center gap-6 border-b-[1px] border-beeci-yellow-600 bg-gradient-to-br from-gray-950 from-10% via-gray-900 via-80% to-beeci-yellow-950 to-100% px-14 pb-10 pt-16 md:w-5/12 md:border-b-0 md:border-r-[1px]'>
         <div>
           <Image
@@ -29,12 +32,14 @@ export const SideBySideLayout = ({ children }: { children: ReactNode }) => (
             efficient, user-friendly, and designed to make your development
             lifecycle buzz 🐝 with productivity.
           </p>
-          <Button className='mt-6 w-1/2'>Begin here </Button>
+          <Button className='mt-6 w-1/2 min-w-max py-4'>
+            <a href={authUrl}>Sign in with GitHub</a>
+          </Button>
         </div>
-        <div className='flex w-full flex-col items-start text-sm md:flex-row md:gap-4 lg:gap-8'>
+        <div className='flex w-full flex-col items-start text-sm lg:flex-row lg:gap-4 lg:gap-8'>
           <Button variant='link' className='px-0'>
             <BookOpen size={18} className='mr-1' />
-            Documentation
+            Read documentation
           </Button>
           <Button variant='link' className='px-0'>
             <a
@@ -43,7 +48,7 @@ export const SideBySideLayout = ({ children }: { children: ReactNode }) => (
               className='flex'
             >
               <GithubIcon className='mr-1 px-0' />
-              Github
+              View on GitHub
             </a>
           </Button>
         </div>
@@ -52,5 +57,5 @@ export const SideBySideLayout = ({ children }: { children: ReactNode }) => (
         <div className='h-full p-4'>{children}</div>
       </div>
     </div>
-  </div>
-);
+  );
+};
