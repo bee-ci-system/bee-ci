f1bd9a7d0bc31236f2eeb5862196536648a0e61d
Bartek Pacia <barpac02@gmail.com>
2024-08-08 23:10:30 +0100

    tailwind config: reset `center` and `padding` from `container` config

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -6,8 +6,6 @@ module.exports = {
   content: ['app/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}'],
   theme: {
     container: {
-      center: true,
-      padding: '2rem',
       screens: {
         '2xl': '1400px',
       },
