90467ac89095eb3a92ba5f5f25faa68adc9095a1
Bartek Pacia <barpac02@gmail.com>
2024-09-25 00:30:12 +0200

    more debug logs

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,32 +3,37 @@ import { NextResponse, type NextRequest } from 'next/server';
 import { documentationRoutes, routes } from './app/_utils/routes';
 
 export function middleware(request: NextRequest) {
-  console.log(
-    `DEBUG middleware: started. request: ${console.log(JSON.stringify(request, null, 4))}`,
-  );
+  console.log(`
+-----
+DEBUG: New request! Content:
+${console.log(JSON.stringify(request, null, 4))}
+-----
+`);
   const { pathname } = request.nextUrl;
 
   if (pathname === '/docs') {
-    console.log(`DEBUG middleware: entered pathname === /docs`);
+    console.log(`DEBUG: entered pathname === /docs`);
     const redirectUrl = new URL(documentationRoutes[0].href, request.url);
-    console.log('redirectUrl', redirectUrl);
+    console.log('DEBUG: redirectUrl:', redirectUrl);
     return NextResponse.redirect(redirectUrl);
   }
 
-  const token = cookies().get('token');
-  console.log(`DEBUG middleware: token cookie: ${token}`);
-  console.log(`DEBUG middleware: jwt cookie: ${cookies().get('jwt')}`);
+  const token = cookies().get('jwt');
+  console.log(`DEBUG: token: ${token}`);
 
   if (!token && pathname !== '/') {
-    console.log(`DEBUG middleware: entered !token && pathname !== '/'`);
+    // For non-authenticated users, always redirect to landing
+    console.log(`DEBUG: entered !token && pathname !== '/'`);
     return NextResponse.redirect(new URL('/', request.url));
   }
 
   if (token && pathname === '/') {
-    console.log(`DEBUG middleware: entered token && pathname === '/'`);
+    // For authenticated users, always redirect from landing to dashboard
+    console.log(`DEBUG: entered token && pathname === '/'`);
     return NextResponse.redirect(new URL(routes.DASHBOARD, request.url));
   }
 
+  console.log(`DEBUG: entered default return. END REQUEST -->`);
   return NextResponse.next();
 }
 
