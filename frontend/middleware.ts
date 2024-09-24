30640215f2a5e2bb5f1996ed0cec9496f476aab7
Bartek Pacia <barpac02@gmail.com>
2024-09-25 00:02:27 +0200

    middleware.ts: add debug console.logs

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,26 +3,27 @@ import { NextResponse, type NextRequest } from 'next/server';
 import { documentationRoutes, routes } from './app/_utils/routes';
 
 export function middleware(request: NextRequest) {
+  console.log(`DEBUG middleware: started. request: ${request}`);
   const { pathname } = request.nextUrl;
 
-  if (pathname === '/log-out') {
-    const response = NextResponse.redirect(new URL('/', request.url));
-    response.cookies.set('jwt', '', { path: '/', expires: new Date(0) });
-    return response;
-  }
-
   if (pathname === '/docs') {
+    console.log(`DEBUG middleware: entered pathname === /docs`);
     const redirectUrl = new URL(documentationRoutes[0].href, request.url);
+    console.log('redirectUrl', redirectUrl);
     return NextResponse.redirect(redirectUrl);
   }
 
-  const token = cookies().get('jwt');
+  const token = cookies().get('token');
+  console.log(`DEBUG middleware: token cookie: ${token}`);
+  console.log(`DEBUG middleware: jwt cookie: ${cookies().get('jwt')}`);
 
   if (!token && pathname !== '/') {
+    console.log(`DEBUG middleware: entered !token && pathname !== '/'`);
     return NextResponse.redirect(new URL('/', request.url));
   }
 
   if (token && pathname === '/') {
+    console.log(`DEBUG middleware: entered token && pathname === '/'`);
     return NextResponse.redirect(new URL(routes.DASHBOARD, request.url));
   }
 
