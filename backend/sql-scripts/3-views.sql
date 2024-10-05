ad2e782f820fb0463e1e8faa87529eb46c2e03cd
Bartek Pacia <barpac02@gmail.com>
2024-10-05 21:41:06 +0200

    create new users_nice view

--- backend/sql-scripts/3-views.sql
+++ backend/sql-scripts/3-views.sql
@@ -2,11 +2,14 @@
 
 SET search_path TO bee_schema, public;
 
+CREATE VIEW users_nice AS
+SELECT users.id, username.name
+FROM bee_schema.users;
+
 CREATE VIEW repos_and_users AS
-SELECT
-    repos.id AS repo_id,
-    repos.name AS repo_name,
-    repos.user_id,
-    users.username
+SELECT repos.id   AS repo_id,
+       repos.name AS repo_name,
+       repos.user_id,
+       users.username
 FROM bee_schema.repos
-JOIN bee_schema.users ON users.id = repos.user_id;
+         JOIN bee_schema.users ON users.id = repos.user_id;
