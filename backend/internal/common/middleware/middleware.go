45a4050536bd31f6f557045c9070686c33f1d979
Bartek Pacia <barpac02@gmail.com>
2024-10-07 21:42:05 +0200

    add cors middleware

--- backend/internal/common/middleware/middleware.go
+++ backend/internal/common/middleware/middleware.go
@@ -30,6 +30,22 @@ func WithTrailingSlashes(next http.Handler) http.Handler {
 	})
 }
 
+func WithCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Allow requests from all origins
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		// w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		// w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func WithLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		logger := slog.With(slog.String("request_id", makeRequestID()))
