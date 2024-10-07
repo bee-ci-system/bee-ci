92cdf4b86529fb54a798d1fe5f06129d7a5c5f5f
Bartek Pacia <barpac02@gmail.com>
2024-10-07 22:36:22 +0200

    update cors

--- backend/internal/common/middleware/middleware.go
+++ backend/internal/common/middleware/middleware.go
@@ -34,8 +34,8 @@ func WithCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Allow requests from all origins
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		// w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		// w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
