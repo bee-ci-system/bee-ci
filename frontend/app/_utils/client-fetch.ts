ef7ca018e83de370c8c25df90b6cb9fe01df4633
Aleksandra Kacprzak <aleksandra.kacprzak@gorrion.io>
2024-10-10 18:47:36 +0200

    fix/fixed-client-fetch

--- frontend/app/_utils/client-fetch.ts
+++ frontend/app/_utils/client-fetch.ts
@@ -19,7 +19,7 @@ export const clientFetch = async (
   });
 
   if (token) {
-    headers.set('Cookie', `jwt=${token.valueOf}`);
+    headers.set('Cookie', `jwt=${token}`);
   }
 
   const response = await fetch(`${baseUrl}${endpoint}`, {
