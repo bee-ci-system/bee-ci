cacd10b1b0632544e0bee4187fd02fea7b99b13a
Bartek Pacia <barpac02@gmail.com>
2024-10-10 23:36:49 +0200

    remove droplet ip output

--- infra-do/outputs.tf
+++ infra-do/outputs.tf
@@ -19,10 +19,9 @@ output "live_domain" {
   value       = digitalocean_app.app.live_domain
 }
 
-# Outputs
-output "droplet_ip" {
-  value = digitalocean_droplet.influxdb.ipv4_address
-}
+# output "droplet_ip" {
+#  value = digitalocean_droplet.influxdb.ipv4_address
+#}
 
 output "volume_id" {
   value = digitalocean_volume.influxdb_volume.id
