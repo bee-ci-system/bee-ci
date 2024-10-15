e9ccade1ad156547367172291ebaeb8be1427b7b
Aleksandra Kacprzak <aleksandra.kacprzak@gorrion.io>
2024-10-15 17:43:08 +0200

    feat/connected-pipeline-logs-endpoint

--- frontend/app/(authorized)/pipeline/[id]/_components/terminal.tsx
+++ frontend/app/(authorized)/pipeline/[id]/_components/terminal.tsx
@@ -1,17 +1,8 @@
 'use client';
 
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@/app/_components/tooltip';
-import { TooltipProvider } from '@radix-ui/react-tooltip';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
-import { useEffect, useRef, useState } from 'react';
-import {
-  getAllPipelineLogsClient,
-  getPipelineLogsClient,
-} from '../_api/client';
+import { useEffect, useRef } from 'react';
+import { getPipelineLogsClient } from '../_api/client';
 
 const TerminalWindow = ({
   pipelineId,
@@ -20,17 +11,19 @@ const TerminalWindow = ({
   pipelineId: string;
   initialLogs: string;
 }) => {
-  const [fetchAllLogs, setFetchAllLogs] = useState(false);
+  // const [fetchAllLogs, setFetchAllLogs] = useState(false);
 
   const { data: logsData } = useQuery<string>({
-    queryKey: ['pipelineLogs', pipelineId, fetchAllLogs],
+    // queryKey: ['pipelineLogs', pipelineId, fetchAllLogs],
+    queryKey: ['pipelineLogs', pipelineId],
     queryFn: () =>
-      fetchAllLogs
-        ? getAllPipelineLogsClient(pipelineId)
-        : getPipelineLogsClient(pipelineId),
+      // fetchAllLogs
+      //   ? getAllPipelineLogsClient(pipelineId)
+      getPipelineLogsClient(pipelineId),
     placeholderData: keepPreviousData,
     initialData: initialLogs,
-    refetchInterval: fetchAllLogs ? undefined : 2000,
+    // refetchInterval: fetchAllLogs ? undefined : 2000,
+    refetchInterval: 2000,
     enabled: true,
   });
 
@@ -47,7 +40,7 @@ const TerminalWindow = ({
   return (
     <div className='h-full w-full'>
       <div className='coding inverse-toggle h-full rounded-lg bg-slate-100 px-5 pb-6 pt-4 font-mono text-sm leading-normal text-foreground subpixel-antialiased shadow-lg dark:bg-gray-800'>
-        <div className='top mb-2 flex'>
+        {/* <div className='top mb-2 flex'>
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
@@ -62,7 +55,7 @@ const TerminalWindow = ({
               </TooltipContent>
             </Tooltip>
           </TooltipProvider>
-        </div>
+        </div> */}
         {logsData === '' ? (
           <div className='flex h-full items-center justify-center'>
             <p className='text-muted-foreground'>No logs</p>
