5ade819ac29750bdf6826513a391fc3621ab820c
Bartek Pacia <barpac02@gmail.com>
2024-10-22 23:32:52 +0200

    debug cloud-inti: start

--- infra-do/droplet-influx.tf
+++ infra-do/droplet-influx.tf
@@ -20,34 +20,9 @@ resource "digitalocean_droplet" "influxdb" {
 
     # From: https://docs.influxdata.com/influxdb/v2/install
     runcmd:
-      - echo "hello from cloud-init! started! pwd is: $(pwd)" >> /root/hello.txt
+      - echo "hello from cloud-init on influx! started! pwd is: $(pwd)" >> /root/hello.txt
 
-      # Install InfluxDB
-      - curl --silent --location -O https://repos.influxdata.com/influxdata-archive.key
-      - echo "943666881a1b8d9b849b74caebf02d3465d6beb716510d86a39f6c8e8dac7515 influxdata-archive.key" | sha256sum --check - && cat influxdata-archive.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null && echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' | tee /etc/apt/sources.list.d/influxdata.list
-      - sudo apt-get update && sudo apt-get install -y influxdb2
-
-      # Mount the volume to /var/lib/influxdb2
-      - mkfs.ext4 /dev/disk/by-id/scsi-0DO_Volume_influxdb-data
-      - mount /dev/disk/by-id/scsi-0DO_Volume_influxdb-data /var/lib/influxdb2
-      - echo "/dev/disk/by-id/scsi-0DO_Volume_influxdb-data /var/lib/influxdb2 ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
-
-      # Create the config folder
-      - mkdir -p /etc/influxdb2
-
-      # Initialize InfluxDB with the provided environment variables
-      - INFLUXDB_INIT_USERNAME=beeci
-      - INFLUXDB_INIT_PASSWORD=${var.influxdb_password}
-      - INFLUXDB_INIT_ADMIN_TOKEN=${var.influxdb_token}
-      - INFLUXDB_INIT_ORG=${var.influxdb_org}
-      - INFLUXDB_INIT_BUCKET=${var.influxdb_bucket}
-
-      - influx setup --username beeci --password ${var.influxdb_password} --token ${var.influxdb_token} --org ${var.influxdb_org} --bucket ${var.influxdb_bucket} --force
-
-      # Restart InfluxDB to apply storage and initialization changes
-      - sudo systemctl enable influxdb
-      - sudo systemctl restart influxdb
-      - echo "hello from cloud-init! done!" >> /root/hello.txt
+    final_message: "influx ready!"
     EOF
 }
 
@@ -57,6 +32,3 @@ resource "digitalocean_volume" "influxdb_volume" {
   region                  = "sfo3"
   initial_filesystem_type = "ext4"
 }
-
-# To check if cloud-init completed successfully, see:
-# https://www.digitalocean.com/community/questions/how-to-make-sure-that-cloud-init-finished-running
