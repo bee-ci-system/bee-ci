51f24484492c392ce447fa599a6257335dee3430
Bartek Pacia <barpac02@gmail.com>
2024-10-25 00:55:23 +0200

    add executor terraform clodu-init config

--- infra-do/droplet-executor.tf
+++ infra-do/droplet-executor.tf
@@ -21,6 +21,31 @@ resource "digitalocean_droplet" "executor" {
     # From: https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04
     runcmd:
       - 'echo "hello from cloud-init on executor! started! pwd is: $(pwd)" >> /root/hello.txt'
+      - 'sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common'
+      - 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg'
+      - 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null'
+      - 'sudo apt-get update'
+      - 'apt-cache policy docker-ce'
+
+      - 'sudo apt-get install -y docker-ce'
+      - 'sudo systemctl status docker'
+
+      - 'docker pull ghcr.io/bee-ci-system/bee-ci/executor:latest'
+
+      # Postgres config
+      - 'echo "DB_HOST=${digitalocean_database_cluster.postgres.host}" >> /root/.executor.env'
+      - 'echo "DB_PORT=${digitalocean_database_cluster.postgres.port}" >> /root/.executor.env'
+      - 'echo "DB_PASSWORD=${digitalocean_database_cluster.postgres.password}" >> /root/.executor.env'
+      - 'echo "DB_NAME=${digitalocean_database_cluster.postgres.name}" >> /root/.executor.env'
+
+      # Influx config
+      - 'echo "INFLUXDB_URL=http://${digitalocean_droplet.influxdb.ipv4_address_private}:8086" >> /root/.executor.env'
+      - 'echo "INFLUXDB_ORG=${var.influxdb_org}" >> /root/.executor.env'
+      - 'echo "INFLUXDB_BUCKET=${var.influxdb_bucket}" >> /root/.executor.env'
+      - 'echo "INFLUXDB_TOKEN=${var.influxdb_token}" >> /root/.executor.env'
+
+      - 'docker run --env-file /root/.executor.env bee-ci-backend-executor:latest'
+      - 'echo "hello from cloud-init! done!" >> /root/hello.txt'
 
     final_message: "executor ready!"
     EOF
