53b3323ed70ffd43b447b89b59ffc36a4905f9cb
Bartek Pacia <barpac02@gmail.com>
2026-04-30 22:33:14 -0700

    ssh into ec2 box

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -26,16 +26,37 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_security_group" "box_sg" {
+  name = "bee-ci-box"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_key_pair" "box" {
   key_name   = "bee-ci-box"
   public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILlmPPetLfPL/eTOI5wLcO3sBiY6wtjhwgm/wlQSd2LP"
 }
 
 resource "aws_instance" "box" {
-  availability_zone = "us-east-1a"
-  instance_type     = "t3.micro"
-  ami               = data.aws_ami.ubuntu.id
-  key_name          = aws_key_pair.box.key_name
+  availability_zone      = "us-east-1a"
+  instance_type          = "t3.micro"
+  ami                    = data.aws_ami.ubuntu.id
+  key_name               = aws_key_pair.box.key_name
+  vpc_security_group_ids = [aws_security_group.box_sg.id]
 }
 
 output "box_public_ip" {
